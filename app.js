3b329a8aacf0aa7f5cd5ed30ad0e4e6c7dbe864d
John Merritt <johnmerritt95@gmail.com>
2020-06-02 16:26:22 -0700

    passes test that came with homework assignment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Intern = require("./lib/Intern").default;
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
